f9c70d1af9352389a5dab427d013126c048b5d69
George Smith <litesoft@gmail.com>
2014-06-06 12:49:19 -0700

    Base Lining.

--- src/org/litesoft/commonfoundation/annotations/Immutable.java
+++
@@ -1,8 +0,0 @@
-package org.litesoft.commonfoundation.annotations;
-
-@java.lang.annotation.Documented
-@java.lang.annotation.Target({java.lang.annotation.ElementType.METHOD, java.lang.annotation.ElementType.PARAMETER})
-public @interface Immutable
-{
-    String value() default "";
-}
